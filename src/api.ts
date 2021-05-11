0475873e0f85086f6f8e12545b126835b2c88bc5
Frank Su <jhihwei.su@gmail.com>
2021-05-12 02:14:41 +0800

    add functions, request and modify unit tests

--- src/api.ts
+++ src/api.ts
@@ -2,15 +2,98 @@ import axios, { AxiosRequestConfig, AxiosPromise, AxiosResponse, AxiosInstance }
 
 interface IConstructor {
   url: string;
+  apiKey: string;
   timeout?: number;
 }
 
-export default class Api {
+interface IRequest {
+  url: string;
+  method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+  data: any;
+}
+
+export default class VoucheryIO {
   client: AxiosInstance;
-  constructor({ url, timeout = 10 * 1000 }: IConstructor) {
+  constructor({ url, apiKey, timeout = 10 * 1000 }: IConstructor) {
     this.client = axios.create({
       baseURL: url,
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
       timeout,
     });
   }
+
+  private async apiRequest({ url, method, data }: IRequest): Promise<any> {
+    let request: AxiosResponse;
+    try {
+      request = await this.client.request(
+        method === 'GET'
+          ? {
+              url,
+              method,
+            }
+          : {
+              url,
+              method,
+              data,
+            },
+      );
+    } catch (error) {
+      let errorMsg: string;
+      if (error.response) {
+        errorMsg = JSON.stringify({
+          status: error.response.status,
+          headers: error.response.headers,
+          message: error.response.data ? JSON.stringify(error.response.data) : null,
+        });
+      } else if (error.request) {
+        errorMsg = JSON.stringify({
+          status: 998,
+          headers: error.request.headers,
+          message: 'Cannot Send Request',
+        });
+      } else {
+        errorMsg = JSON.stringify({
+          status: 999,
+          headers: null,
+          message: 'Unexpected error',
+        });
+      }
+
+      throw new Error(errorMsg);
+    }
+
+    return request.data;
+  }
+
+  createCampaign() {}
+
+  updateCampaign() {}
+
+  deleteCampaign() {}
+
+  getCampaign() {}
+
+  allRewardsInCampaign() {}
+
+  createReward() {}
+
+  updateReward() {}
+
+  deleteReward() {}
+
+  allVouchersInCampaign() {}
+
+  createVoucher() {}
+
+  deleteVoucher() {}
+
+  getVoucher() {}
+
+  createRedemption() {}
+
+  confirmRedemption() {}
+
+  getRedemption() {}
 }
--- test/api.test.ts
+++ test/api.test.ts
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv';
-import api from '../src/api';
+import VoucheryIO from '../src/api';
 
 dotenv.config();
+
+const voucheryIO = new VoucheryIO({
+  url: process.env.VOUCHERY_IO_BASE_URL,
+  apiKey: process.env.VOUCHERY_IO_API_KEY,
+});
 
 describe('example test', () => {
   it('should be truthy', () => {
